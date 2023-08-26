7614d3faa03ff57a810728729812174038625357
P-Alex13 <109438791+P-Alex13@users.noreply.github.com>
2023-08-26 19:01:40 +0000

    Deploying to gh-pages from @ P-Alex13/project-team3-ice-cream@4bada189001fc803a103e1adaab76244364ce365 🚀

--- index.7d2acc92.js
+++ index.7d2acc92.js
@@ -1,2 +1,2 @@
 (()=>{const o={openModalBtn:document.querySelector("[data-modal-open-location]"),closeModalBtn:document.querySelector("[data-modal-close-location]"),modal:document.querySelector("[data-modal-location]")};function e(){o.modal.classList.toggle("is-hidden-location")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)})();
-//# sourceMappingURL=index.91ef2b64.js.map
+//# sourceMappingURL=index.7d2acc92.js.map
